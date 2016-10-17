15be2663534195999f9150aaa5d405384c67538c
emanzelekha <emanzelekha@gmail.com>
2016-10-17 06:08:24 -0700

    third

--- app/src/main/java/com/example/eman/anew/MainActivity.java
+++ app/src/main/java/com/example/eman/anew/MainActivity.java
@@ -9,8 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //ghfgtrfdtrdfr
     }
-   public void test(){
-       int k=0;
-   }
+
 }
--- app/src/main/java/com/example/eman/anew/play.java
+++
@@ -1,7 +0,0 @@
-package com.example.eman.anew;
-
-/**
- * Created by Eman on 10/17/2016.
- */
-public class play {
-}
